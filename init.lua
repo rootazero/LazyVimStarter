45e6ec89c19a25e6bed03e42e68e52b2304bb120
rootazero <124485722+rootazero@users.noreply.github.com>
2025-12-04 22:02:03 +0800

    Change comment for Python environment setup
    Updated comment to reflect the use of uv for setting Python environment.

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- 通过conda设置python环境
+-- 通过uv设置python环境
 if vim.loop.os_uname().sysname == "Darwin" or vim.loop.os_uname().sysname == "Linux" then
   vim.g.python_host_prog = "$HOME/.uv/nvim/bin/python"
   vim.g.python3_host_prog = "$HOME/.uv/nvim/bin/python"
