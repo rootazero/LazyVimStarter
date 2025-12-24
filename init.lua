89cc019d044a5fa92b9e8adc923b67b6be00ea5f
rootazero <124485722+rootazero@users.noreply.github.com>
2025-12-24 10:35:41 +0800

    Update Python host program paths for environments

--- init.lua
+++ init.lua
@@ -3,11 +3,11 @@ require("config.lazy")
 
 -- 通过uv设置python环境
 if vim.loop.os_uname().sysname == "Darwin" or vim.loop.os_uname().sysname == "Linux" then
-  vim.g.python_host_prog = "$HOME/.uv/nvim/.venv/bin/python"
-  vim.g.python3_host_prog = "$HOME/.uv/nvim/.venv/bin/python"
+  vim.g.python_host_prog = "$HOME/.uv/nvim/bin/python"
+  vim.g.python3_host_prog = "$HOME/.uv/nvim/bin/python"
 elseif vim.loop.os_uname().sysname == "Windows_NT" then
-  vim.g.python_host_prog = "%USERPROFILE%\\.uv\\nvim\\.venv\\bin\\python"
-  vim.g.python3_host_prog = "%USERPROFILE%\\.uv\\nvim\\.venv\\bin\\python"
+  vim.g.python_host_prog = "%USERPROFILE%\\.uv\\nvim\\bin\\python"
+  vim.g.python3_host_prog = "%USERPROFILE%\\.uv\\nvim\\bin\\python"
 end
 
 -- visual 模式下，按 Ctrl+c 直接复制到系统剪切板
